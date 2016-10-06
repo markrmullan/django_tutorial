d844b0aa5933fa1998ecd0979c2f76c13d06cd50
Mark Mullan <markrmullan@gmail.com>
2016-10-06 11:28:26 -0700

    customize admin question form

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 from .models import Question
 
 # Register your models here.
-admin.site.register(Question)
+# admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
+# create an admin model class, then pass it as the second argument to admin.site.register(),
+# any time you need to change the admin options for a model
+# good for re-ordering fields on particularly complex forms, for good readability and usability
