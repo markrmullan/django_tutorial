c270969a57585cc6cfe580adc51ad6f8a0c5be51
Mark Mullan <markrmullan@gmail.com>
2016-10-06 10:14:00 -0700

    first automated tests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
 
+from .models import Question
 # Create your tests here.
+
+class QuestionMethodTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        # should return False for questions with a pub_date in the future
+
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date = time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        # should return False for question with a pub_date older than 1 day
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date = time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
